425974ca7e4dca9b51e26388f767293016d8c065
Kawan Weber <kawan_weber@hotmail.com>
2025-04-06 20:44:56 -0300

    Attkm

---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿Console.Write("Entre com a medida (em milhas): ");
+double milhas = Convert.ToDouble(Console.ReadLine());
+
+double quilometros = milhas * 1.609;
+
+Console.WriteLine($"{quilometros:N3} Km");
